ac0b0b46dc0541706841c4f587e6569e6f0cd5fb
Joe Decock <c_jdecock@healthequity.com>
2021-07-13 11:16:30 -0500

    Expanded keyof examples

--- src/3.keyof.ts
+++ src/3.keyof.ts
@@ -12,8 +12,8 @@ var name = get(joe, 'name');
 // TODO - Try making typos ("naem") and observe the type error
 
 // Types can be single values
-type K1 = 'name';
-type K2 = 'birthday';
+type Key1 = 'name';
+type Key2 = 'birthday';
 
 // You can refer to the names of the properties of an object as a type.
 // I want to get the keys of the Person type, that is, the type
@@ -21,3 +21,26 @@ type keysOfPerson = 'name' | 'birthday';
 
 // There is an operator that does that:
 type keysOfPersonEasier = keyof Person;
+
+// Generic types can be constrained with the extends keyword
+interface Worker extends Person {
+  company: string;
+}
+function genericWithExtends<T extends Person>() {}
+
+genericWithExtends<Person>();
+genericWithExtends<Worker>();
+
+// P extends P | Q | R
+function needsAPropertyOfPerson<T extends keyof Person>() {}
+needsAPropertyOfPerson<'name'>();
+needsAPropertyOfPerson<'birthday'>();
+// Technically you *could* do this
+needsAPropertyOfPerson<'name' | 'birthday'>();
+// But this is a type error
+needsAPropertyOfPerson<'something else'>();
+
+// You can also refer to the type of a property
+// using the "indexed access operator".
+// This is called a "lookup type"
+type birthdaysType = Person['birthday'];
